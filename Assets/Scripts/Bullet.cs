db6e9756eb77de070b26af82c8f3bc1d4cfa3631
m305_1\m305 <a110082040@mail.shu.edu.tw>
2022-11-02 12:44:04 +0800

    boy i have no clue.....

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 30f;
+    public float atk = 20;
     private Rigidbody rb;
 
     void Start()
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float hp =120;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+         if(other.tag == "bullet")
+         {
+             Bullet bullet = other.GetComponent<Bullet>();
+            hp-=bullet.atk;
+
+            if(hp<=0)
+            {
+            gameObject.SetActive(false);
+            //Destroy(gameObject);
+            }
+         }
+    }
+}
+
+
